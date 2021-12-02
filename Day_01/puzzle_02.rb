157a8ea5ddaae9b03121b5b3088b79a4ff2f85ee
Skylar Niccum <benryan1300@gmail.com>
2021-12-02 01:31:20 -0800

    Revamp Day 01 - Puzzle 2
    Now same as Puzzle 1, but with a different window size

--- Day_01/puzzle_02.rb
+++ Day_01/puzzle_02.rb
@@ -1,53 +1,17 @@
 #!/usr/bin/env ruby
+require './lib/scanner'
 
-FILENAME = "input_01.txt"
+scanner = Scanner.new(filename: "Day_01/input_01.txt", window_size: 3)
 
-class Scanner
-  def initialize(filename)
-    @input = File.open(filename, 'r')
-    @current_window = []
-  end
-
-  def more_data?
-    !@input.eof?
-  end
-
-  def next_reading
-    read_data
-
-    return @current_window.sum if full_window?
-
-    return nil
-  end
-
-  private
-
-  def full_window?
-    @current_window.size == 3
-  end
-
-  def read_data
-    @current_window.shift if full_window?
-
-    while more_data? && !full_window? do
-      @current_window << @input.readline.to_i
-    end
-  end
-end
-
-scanner = Scanner.new(FILENAME)
-
-last_reading = nil
 times_increased = 0
+last_window_reading = nil
 
-while scanner.more_data? do
-  current_reading = scanner.next_reading
+while scanner.has_data? do
+  current_window_reading = scanner.read_window.sum
 
-  puts current_reading
+  times_increased += 1 if !last_window_reading.nil? && current_window_reading > last_window_reading
 
-  times_increased += 1 if !last_reading.nil? && current_reading > last_reading
-
-  last_reading = current_reading
+  last_window_reading = current_window_reading
 end
 
-puts "Readings increased a total of #{times_increased} times"
+puts "Scanner readings increased a total of #{times_increased} times"
