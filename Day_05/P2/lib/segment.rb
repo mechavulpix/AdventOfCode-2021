2c782e29d9628f7dd75c4bc89f323e4f41ab98cb
Skylar Niccum <benryan1300@gmail.com>
2021-12-05 02:11:09 -0800

    Day 05 - Puzzle 10

---
+++ Day_05/P2/lib/segment.rb
@@ -0,0 +1,81 @@
+class Segment
+
+  def initialize(start_x:, start_y:, finish_x:, finish_y:)
+    @start = Point.new(x: start_x, y: start_y)
+    @finish = Point.new(x: finish_x, y: finish_y)
+
+    @delta_x = (finish_x - start_x).abs
+    @coefficient_x = (finish_x - start_x).negative? ? -1 : 1
+    @delta_y = (finish_y - start_y).abs
+    @coefficient_y = (finish_y - start_y).negative? ? -1 : 1
+  end
+
+  def orientation
+    return :vertical if @start.x == @finish.x
+    return :horizontal if @start.y == @finish.y
+    return :diagonal
+  end
+
+  def max_x
+    [@start.x, @finish.x].max
+  end
+
+  def max_y
+    [@start.y, @finish.y].max
+  end
+
+  def as_point_array
+    points = []
+
+    if (orientation == :horizontal)
+      points << @start
+      @delta_x.times do |x|
+        offset_x = (x + 1) * @coefficient_x
+        points << Point.new(x: @start.x + offset_x, y: @start.y)
+      end
+    end
+
+    if (orientation == :vertical)
+      points << @start
+      @delta_y.times do |y|
+        offset_y = (y + 1) * @coefficient_y
+        points << Point.new(x: @start.x, y: @start.y + offset_y)
+      end
+    end
+
+    if (orientation == :diagonal)
+      points << @start
+      @delta_x.times do |d|
+        offset_x = (d + 1) * @coefficient_x
+        offset_y = (d + 1) * @coefficient_y
+        points << Point.new(x: @start.x + offset_x, y: @start.y + offset_y)
+      end
+    end
+
+    points
+  end
+
+  def self.parse(segment_string:)
+    points = segment_string.split("->")
+    start = points.first.strip.split(",")
+    finish = points.last.strip.split(",")
+
+    Segment.new(
+      start_x: start.first.to_i,
+      start_y: start.last.to_i,
+      finish_x: finish.first.to_i,
+      finish_y: finish.last.to_i)
+  end
+
+  private
+
+  class Point
+    attr_accessor :x
+    attr_accessor :y
+
+    def initialize(x:, y:)
+      @x = x
+      @y = y
+    end
+  end
+end
---
+++ Day_05/P2/puzzle_10.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+require_relative 'lib/segment'
+require_relative 'lib/terrain'
+
+data_file = File.open("Day_05/input_05.txt", 'r')
+
+segments = []
+
+until data_file.eof? do
+  segments << Segment.parse(segment_string: data_file.readline.strip)
+end
+
+rows = segments.map(&:max_y).max + 1
+columns = segments.map(&:max_x).max + 1
+
+terrain = Terrain.new(rows: rows, columns: columns)
+
+segments.each do |segment|
+  terrain.add_segment(segment: segment)
+end
+
+puts "Terrain has #{terrain.get_overlap_point_total} points of overlap"
