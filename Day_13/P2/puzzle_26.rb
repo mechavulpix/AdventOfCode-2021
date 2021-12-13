61007403dbc5f4a0cb610a72ebb3884d15540f96
Skylar Niccum <benryan1300@gmail.com>
2021-12-12 23:04:57 -0800

    Day 13 - Puzzle 26
    Over half way!

---
+++ Day_13/P2/puzzle_26.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+require_relative 'lib/paper'
+
+data_file = File.open("Day_13/input_13.txt", 'r')
+
+dots = []
+instructions = []
+
+input_line = data_file.readline.strip
+until input_line.empty? do
+  dots << input_line.split(",").map(&:to_i)
+  input_line = data_file.readline.strip
+end
+
+until data_file.eof? do
+  instructions << Instruction.new(instruction_string: data_file.readline.strip)
+end
+
+paper = Paper.new(dots: dots)
+
+instructions.each do |instruction|
+  paper.perform(instruction: instruction)
+end
+
+puts paper
